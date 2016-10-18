7ffe0433f1a92175cb16278e782ccaeb2505aba1
Harry Sutcliffe <h.sutcliffe@live.com>
2016-10-18 00:36:19 +0000

    added destroy to controller

--- app/controllers/at_risk_controller.rb
+++ app/controllers/at_risk_controller.rb
@@ -1,5 +1,12 @@
 class AtRiskController < ApplicationController
     def index
     end
+    
+    def destroy
+        @form = Form.find(params[:id])
+        @form.form_activeness = false
+        flash[:notice] = "Form was deleted."
+        redirect_to forms_path
+    end
 end
 
