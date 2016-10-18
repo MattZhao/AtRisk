ff5aedb825ccb9d606817b0eed0d6e1dd477bdeb
Huu Pham <htphamschool@gmail.com>
2016-10-18 01:12:04 +0000

    finished Autism Alert Form

--- app/controllers/at_risk_controller.rb
+++ app/controllers/at_risk_controller.rb
@@ -1,16 +1,16 @@
 class AtRiskController < ApplicationController
-    def index
-    end
+  def index
+  end
     
-    def form_params
-      #params.require(#include all non-optional params here)
-    end
+  def form_params
+  #params.require(#include all non-optional params here)
+  end
     
     
-    def create
-      @form = Form.create!(form_params)
-      flash[:notice] = 'Your form is submitted'
-      redirect_to forms_path
-    end
+  def create
+    @form = Form.create!(form_params)
+    flash[:notice] = 'Your form is submitted'
+    redirect_to forms_path
+  end
 end
 
