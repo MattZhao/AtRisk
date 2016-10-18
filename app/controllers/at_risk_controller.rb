e80f403cb997c968f6a337c0664f5df58d5ab594
Sir Milton <xiangyu.ren@hotmail.com>
2016-10-18 21:13:25 +0000

    added form resource to routes, created one dummy data, implemented show

--- app/controllers/at_risk_controller.rb
+++ app/controllers/at_risk_controller.rb
@@ -8,4 +8,10 @@ class AtRiskController < ApplicationController
     end
   end
 
+  def show
+    id = params[:id] # retrieve form ID from URI route
+    @form = Form.find(id) # look up form by unique ID
+    # will render app/views/forms/show.<extension> by default ???? <== need to test this
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :forms
   devise_for :users
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   root "at_risk#index"
