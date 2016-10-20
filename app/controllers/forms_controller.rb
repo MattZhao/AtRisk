a13857fe6589286c7d8d2fa30ed0b075ad8b7c35
Harry Sutcliffe <h.sutcliffe@live.com>
2016-10-20 00:47:11 +0000

    merge

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -21,4 +21,11 @@ class FormsController < ApplicationController
     redirect_to forms_path
   end
 
+  def destroy
+    @form = Form.find(params[:id])
+    @form.form_activeness = false
+    flash[:notice] = "Form was deleted."
+    redirect_to forms_path
+  end
+
 end
