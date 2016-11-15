07f89360e8656610a9e5430ee1da820ffbca6534
Harry Sutcliffe <h.sutcliffe@live.com>
2016-11-15 05:27:46 +0000

    added tips section

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,8 +1,11 @@
 class Form < ActiveRecord::Base
-    def self.search(search)
-      where("name LIKE ?", "%#{search}%") 
-    end
-    def self.all_types
-      %w(AtRisk Autism)
-    end
+  def self.search(search)
+    where("name LIKE ?", "%#{search}%") 
+  end
+  def self.all_types
+    %w(AtRisk Autism)
+  end
+  #check that terms of service were accepted
+  validates :terms_of_service, :acceptance => {:accept => true}, :allow_nil => false
+
 end
