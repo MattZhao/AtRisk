18730e6221a5aad28d8be975d18f19e6588820f9
Harry Sutcliffe <h.sutcliffe@live.com>
2016-12-01 03:30:30 +0000

    failing cuc

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116084141) do
+ActiveRecord::Schema.define(version: 20161201013259) do
 
   create_table "form_attachments", force: :cascade do |t|
     t.integer  "form_id"
