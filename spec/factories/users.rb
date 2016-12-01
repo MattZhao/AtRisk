6ea36de1f80046812b3b06770ed47721135d4330
Harry Sutcliffe <h.sutcliffe@live.com>
2016-12-01 23:42:57 +0000

    some cucumber and rspec fixes

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,6 +8,6 @@ FactoryGirl.define do
     password_confirmation 'password'
     phone '555555555'
     # required if the Devise Confirmable module is used
-    # confirmed_at Time.now
+    confirmed_at Time.now
   end
 end
